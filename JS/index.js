f31dcfc928b328afb5fc9580c27383e579946563
APuckett <86848741+Gamork@users.noreply.github.com>
2022-02-22 17:49:15 -0500

    Add JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
+console.log("Your index.js file is loaded correctly!");
+
 $("#logoAndy").click(function(){
     alert("ANDY's LOGO! It is a mash of my name. One day, I would like it to animate by starting with ANDY and then merging into the logo.");
   });
